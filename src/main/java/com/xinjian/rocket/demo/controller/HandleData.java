6c3302d9c5d37d9ca0379316a91ddb3f0c7e02e6
rundo <3317729299@qq.com>
2023-12-29 17:19:03 +0800

    自定义日志级别 指定类输出到某一个文件

--- src/main/java/com/xinjian/rocket/demo/controller/HandleData.java
+++ src/main/java/com/xinjian/rocket/demo/controller/HandleData.java
@@ -20,24 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
 //http://localhost:8080/click?ip=123&gender=0&age=22&username=tanrundong&location=yunnan&accountId=9990395
 public class HandleData {
-   // private static Logger log = LoggerFactory.getLogger("HandleData");
-
-    private Logger log = LogManager.getLogger(HandleData.class.getName());
-
-
     private org.apache.logging.log4j.Logger logself= LogManager.getLogger(HandleData.class.getName());
-
-
-
-
-
-
-
 
     @RequestMapping("/click")
     public void click(@RequestParam("ip") String ip,
@@ -47,39 +36,46 @@ public class HandleData {
                       @RequestParam("location") String location,
                       @RequestParam("accountId") String accountId) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
         //写入内存
-
-        List<UserInfo> users = new ArrayList<UserInfo>();
+        //Queue LinkedList存入数据
+        List<UserInfo> users = new LinkedList<UserInfo>();
         UserInfo userInfo = new UserInfo(ip, gender, age, username, location, accountId);
         users.add(new UserInfo(ip, gender, age, username, location, accountId));
-
-
-
-
-        // 创建一个Gson对象
-        Gson gson = new Gson();
-        // 将Java对象转换为JSON字符串
-        String json = gson.toJson(userInfo);
-
-        System.out.println(json); // 输出: {"name":"John","age":30}
-        // 给mq 发消息
-        DefaultMQProducer producer = new DefaultMQProducer("order_group_producer");
-      // producer.setNamesrvAddr("120.77.217.160:9876");
-       producer.setNamesrvAddr("192.168.1.63:30100");
-        producer.setVipChannelEnabled(false);// 生产者和消费者添加这行代买
-        //192.168.1.63:30100
-        producer.start();
-
-
-        Message message = new Message("userInfo", accountId, "KEY", json.getBytes(StandardCharsets.UTF_8));
-
-        logself.log(Level.getLevel("CUSTOMER"),json);
-        //发送消息
-        SendResult result = producer.send(message,100000);
-        //发送状态
-        System.out.println(result.getSendStatus());
-
-
-        producer.shutdown();
+       // System.out.println(" users.size() " +users.size());
+         //新开一个线程 处理数据
+        Thread thread = new Thread(() -> {
+            // 创建一个Gson对象
+            Gson gson = new Gson();
+            // 将Java对象转换为JSON字符串
+            String json = gson.toJson(userInfo);
+            System.out.println(json); // 输出: {"name":"John","age":30}
+            // 给mq 发消息
+            DefaultMQProducer producer = new DefaultMQProducer("order_group_producer");
+            // producer.setNamesrvAddr("120.77.217.160:9876");
+            producer.setNamesrvAddr("192.168.1.63:30100");
+            producer.setVipChannelEnabled(false);// 生产者和消费者添加这行代买
+            //192.168.1.63:30100
+            try {
+                producer.start();
+            } catch (MQClientException e) {
+                e.printStackTrace();
+            }
+            Message message = new Message("userInfo", accountId, "KEY", json.getBytes(StandardCharsets.UTF_8));
+            logself.log(Level.getLevel("CUSTOMER"),json);
+            //发送消息
+            SendResult result = null;
+            try {
+                result = producer.send(message,100000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //发送状态
+            System.out.println(result.getSendStatus());
+            producer.shutdown();
+            //处理完之后 移除该数据
+            users.remove(0);
+        });
+        //调用线程
+        thread.start();
     }
 
     // 消费者 接收消息
